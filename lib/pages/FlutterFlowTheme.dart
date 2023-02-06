13b7c05e48f1c739790234794ce69f0bb8081ceb
DihiaMezghiche <mezghiche.dihia@gmail.com>
2023-02-06 20:20:02 +0100

    first commit

---
+++ lib/pages/FlutterFlowTheme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color kPrimaryColor = Color(0xFF4B39EF);
+const title2 = TextStyle(
+  color: Colors.white,
+  fontSize: 22,
+);
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:vtc_app/pages/FlutterFlowTheme.dart';
+
+class HomePageWidget extends StatefulWidget {
+  const HomePageWidget({Key? key}) : super(key: key);
+
+  @override
+  _HomePageWidgetState createState() => _HomePageWidgetState();
+}
+
+class _HomePageWidgetState extends State<HomePageWidget> {
+  final _unfocusNode = FocusNode();
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void dispose() {
+    _unfocusNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: scaffoldKey,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: kPrimaryColor,
+        automaticallyImplyLeading: false,
+        title: Text('Page Title', style: title2),
+        actions: [],
+        centerTitle: false,
+        elevation: 2,
+      ),
+      body: SafeArea(
+        child: GestureDetector(
+          onTap: () => FocusScope.of(context).requestFocus(_unfocusNode),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [],
+          ),
+        ),
+      ),
+    );
+  }
+}
